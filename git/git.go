1b97da8d75c311b9ee6dce89b5417da84a069a0e
Pablo Carranza <pcarranza@gmail.com>
2018-05-12 23:34:02 +0200

    Extracted git url parsing to package

---
+++ git/git.go
@@ -0,0 +1,59 @@
+package git
+
+import (
+	"errors"
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+)
+
+// URL is an interface that represents a git url
+type URL interface {
+	ToGoPath() string
+}
+
+// ErrInvalidURL is returned from Parse when the url is not a valid git url
+var ErrInvalidURL = errors.New("Invalid URL")
+
+var gitURLParser = regexp.MustCompile("git@([\\w\\.]+):(.+)\\.git")
+var gitPathParser = regexp.MustCompile("(.+)\\.git")
+
+// Parse gets a url string and returns a URL object, or an error
+func Parse(uri string) (URL, error) {
+	if uri == "" {
+		return nil, ErrInvalidURL
+	}
+	if strings.HasPrefix(uri, "git@") {
+		if !gitURLParser.MatchString(uri) {
+			return nil, ErrInvalidURL
+		}
+
+		matches := gitURLParser.FindStringSubmatch(uri)
+		return gitURL{
+			domain: matches[1],
+			path:   matches[2],
+		}, nil
+	}
+	if strings.HasPrefix(uri, "http://") || strings.HasPrefix(uri, "https://") {
+		u, err := url.Parse(uri)
+		if err != nil {
+			return gitURL{}, nil
+		}
+
+		return gitURL{
+			domain: u.Hostname(),
+			path:   gitPathParser.FindStringSubmatch(u.Path)[1],
+		}, nil
+	}
+	return gitURL{}, nil
+}
+
+type gitURL struct {
+	domain string
+	path   string
+}
+
+func (g gitURL) ToGoPath() string {
+	return path.Join(g.domain, g.path)
+}
---
+++ git/git_test.go
@@ -0,0 +1,59 @@
+package git_test
+
+import (
+	"gitlab.com/pcarranza/sh-tools/git"
+	"reflect"
+	"testing"
+)
+
+func Test_ParseGitHubURL(t *testing.T) {
+	tt := []struct {
+		name           string
+		url            string
+		expectedGoPath string
+		expectedError  error
+	}{
+		{
+			"Invalid URL",
+			"",
+			"",
+			git.ErrInvalidURL,
+		},
+		{
+			"GitHub Git URL",
+			"git@github.com:gomeeseeks/meeseeks-box.git",
+			"github.com/gomeeseeks/meeseeks-box",
+			nil,
+		},
+		{
+			"GitHub HTTP URL",
+			"https://github.com/gomeeseeks/meeseeks-box.git",
+			"github.com/gomeeseeks/meeseeks-box",
+			nil,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			url, err := git.Parse(tc.url)
+			assertEquals(t, tc.expectedError, err)
+			if err == nil {
+				assertEquals(t, tc.expectedGoPath, url.ToGoPath())
+			}
+		})
+	}
+}
+
+func assertEquals(t *testing.T, expected, actual interface{}) {
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Value %s is not as expected %s", actual, expected)
+	}
+}
+
+func assertErr(t *testing.T, expected, actual error) {
+	if expected != nil && actual != nil {
+		assertEquals(t, expected.Error(), actual.Error())
+	} else if expected != nil || actual != nil {
+		t.Fatalf("Error %s is not as expected %s", actual, expected)
+	}
+}
